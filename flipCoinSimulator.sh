70af584455ed0a2f62071c10e277f7b31d4054c1
suvodeep4119 <suvodeep4119@gmail.com>
2020-01-29 22:40:25 +0530

    ADDED: Feature to store Singlet Combination and it's %

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,11 +2,37 @@
 
 echo "Welcome to Flip Coin Simulator"
 
-randomValue=$((RANDOM%2))
-if [ $randomValue -eq 1 ]
-then
-	echo "HEADS"
-else
-	echo "TAILS"
-fi
+#ASSUMPTIONS: 1= HEADS 0=TAILS
+
+#DICTIONARY
+declare -A singlet=( [0]=0 [1]=0 )
+
+#VARIABLES
+multiple=0
+
+read -p "Please enter a value in multiples of 2: " multiple
+
+function singlet(){
+	local randomValue=$((RANDOM%2))
+	echo $randomValue
+}
+
+while (( i<multiple ))
+do
+	r=$(singlet)
+	if [ $r -eq 1 ]
+	then
+		singlet[$r]=$(( ${singlet[$r]}+1 ))
+	else
+		singlet[$r]=$(( ${singlet[$r]}+1 ))
+	fi
+	(( i++ ))
+done
+
+#Percentage Calculation Singlet
+percentCombination=`echo "scale=2; ${singlet[1]}*100/$multiple" | bc`
+echo "% of HEADS occurence: " $percentCombination
+percentCombination=`echo "scale=2; ${singlet[0]}*100/$multiple" | bc`
+echo "% of TAILS occurence: " $percentCombination
+
 
