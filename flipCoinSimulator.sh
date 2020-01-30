86571fc24aeaa8b8c7061d07d27a3474a73f01c2
suvodeep4119 <suvodeep4119@gmail.com>
2020-01-30 07:42:26 +0530

    ADDED:Feature to storeTriplet Combination & Calculate %

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -7,6 +7,7 @@ echo "Welcome to Flip Coin Simulator"
 #DICTIONARY
 declare -A singlet=( [0]=0 [1]=0 )
 declare -A doublet=( [00]=0 [01]=0 [10]=0 [11]=0 )
+declare -A triplet=( [000]=0 [001]=0 [011]=0 [111]=0 [101]=0 [110]=0 [100]=0 )
 
 #VARIABLES
 multiple=0
@@ -22,6 +23,14 @@ function doublet(){
 	local r1=$((RANDOM%2))
 	local r2=$((RANDOM%2))
 	local randomValue="${r1}${r2}"
+	echo $randomValue
+}
+
+function triplet(){
+	local r1=$((RANDOM%2))
+	local r2=$((RANDOM%2))
+	local r3=$((RANDOM%2))
+	local randomValue="${r1}${r2}${r3}"
 	echo $randomValue
 }
 
@@ -57,6 +66,36 @@ do
 		echo "Nothing"
 		;;
 	esac
+	r=$(triplet)
+	case $r in
+		000)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		001)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		011)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		111)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		100)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		110)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		010)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		101)
+		triplet[$r]=$(( ${triplet[$r]}+1 ))
+		;;
+		*)
+		echo "Nothing"
+		;;
+	esac
 	(( i++ ))
 done
 
@@ -70,12 +109,27 @@ echo "% of TAILS occurence: " $percentCombination
 #Percentage Calculation Doublet
 percentCombination=`echo "scale=2; ${doublet[00]}*100/$multiple" | bc`
 echo "TT %:" $percentCombination
-
 percentCombination=`echo "scale=2; ${doublet[11]}*100/$multiple" | bc`
 echo "HH %:" $percentCombination
-
 percentCombination=`echo "scale=2; ${doublet[10]}*100/$multiple" | bc`
 echo "HT %:" $percentCombination
-
 percentCombination=`echo "scale=2; ${doublet[01]}*100/$multiple" | bc`
 echo "TH %:" $percentCombination
+
+#Percentage Calculation Triplet
+percentCombination=`echo "scale=2; ${triplet[111]}*100/$multiple" | bc`
+echo "HHH %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[000]}*100/$multiple" | bc`
+echo "TTT %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[100]}*100/$multiple" | bc`
+echo "HTT %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[110]}*100/$multiple" | bc`
+echo "HHT %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[101]}*100/$multiple" | bc`
+echo "HTH %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[010]}*100/$multiple" | bc`
+echo "THT %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[011]}*100/$multiple" | bc`
+echo "THH %:" $percentCombination
+percentCombination=`echo "scale=2; ${triplet[001]}*100/$multiple" | bc`
+echo "TTH %:" $percentCombination
